6bb50d5127bd265edfcc6027c9391e24fcf6946b
Yang Chong <ychong@csod.com>
2020-08-29 10:34:39 -0700

    Initial deployment of empty pipeline.

---
+++ bin/pipeline-initialize.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { App } from '@aws-cdk/core';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+const app = new App();
+
+new PipelineStack(app, 'PipelineStack');
+
+app.synth();
